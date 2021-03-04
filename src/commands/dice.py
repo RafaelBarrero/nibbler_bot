9a01f2cda6d7f6a2b7033e322588c0a1332de226
RafaelBarrero <49143852+RafaelBarrero@users.noreply.github.com>
2021-03-05 00:08:53 +0100

    dice.py: Python file to !d command

---
+++ src/commands/dice.py
@@ -0,0 +1,32 @@
+from discord.ext import commands
+from discord.ext.commands import Context
+import random
+
+
+class Dice(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='d', help='Lanza un dado. Argumentos: Caras y número de veces')
+    async def roll_dice(self, ctx: Context, *args):
+        count = 1
+        if args:
+            dice = int(args[0])
+            if len(args) == 2:
+                tries = (int(args[1]) if int(args[1]) <= 50 else 50)
+            else:
+                tries = 1
+            number_list = []
+            while count <= tries:
+                number = random.randint(1, dice)
+                number_list.append(number)
+                count += 1
+            str_number = ', '.join([f"{number}" for number in number_list])
+            await ctx.send(str_number)
+        else:
+            await self.roll_dice(ctx, 6)
+
+
+def setup(bot):
+    bot.add_cog(Dice(bot))
