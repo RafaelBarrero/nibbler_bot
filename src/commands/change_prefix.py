33a85e638e50bfebf500d2769bcbc38b5ed4993e
RafaelBarrero <49143852+RafaelBarrero@users.noreply.github.com>
2021-03-05 00:08:37 +0100

    change_prefix.py: Python file to !prefijo command

---
+++ src/commands/change_prefix.py
@@ -0,0 +1,36 @@
+import json
+
+import pathlib
+from discord.ext import commands
+from discord.ext.commands import Context, has_permissions, MissingPermissions
+
+
+class ChangePrefix(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.PATH = pathlib.Path(__file__).parent.parent.parent
+
+    @commands.command(name='prefijo', help='Cambia el prefijo')
+    @has_permissions(administrator=True)
+    async def prefix(self, ctx: Context, new_prefix: str):
+        with open(f'{self.PATH}/prefixes.json', 'r') as f:
+            prefixes = json.load(f)
+
+        prefixes[str(ctx.guild.id)] = new_prefix
+
+        with open(f'{self.PATH}/prefixes.json', 'w') as f:
+            json.dump(prefixes, f, indent=4)
+
+        await self.bot.user.edit(display_name="asds")
+        await ctx.send(f"Listo. Prefijo cambiado a \"{new_prefix}\" :D")
+
+    @prefix.error
+    async def kick_error(self, ctx: Context, error):
+        if isinstance(error, MissingPermissions):
+            mention = ctx.message.author.mention
+            await ctx.send(f"Lo siento {mention}, no tienes permisos para eso :(")
+
+
+def setup(bot):
+    bot.add_cog(ChangePrefix(bot))
