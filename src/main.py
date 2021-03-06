9f3f2e62260226dedc8bf671a326f3439a4c6150
RafaelBarrero <ramag874@gmail.com>
2021-03-06 15:31:29 +0100

    main.py: prefixes.json is now on Dropbox

--- src/main.py
+++ src/main.py
@@ -9,14 +9,20 @@ import discord
 import pathlib as pathlib
 from discord.ext import commands
 
+import dropbox
+from dropbox.files import WriteMode
+
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+DROPBOX_TOKEN = os.getenv('DROPBOX_TOKEN')
 GUILD = "SALA BORRACHERAS"
 PATH = pathlib.Path(__file__).parent.parent
 intents = discord.Intents.all()
 guild_found: discord.Guild = None
+
+dbx = dropbox.Dropbox(DROPBOX_TOKEN)
 
 links = []
 
@@ -41,6 +47,10 @@ bot = commands.Bot(command_prefix=when_mentioned_or_function(get_prefix), case_i
 
 @bot.event
 async def on_guild_join(guild: discord.Guild):
+    with open(PATH.joinpath("prefixes.json"), "wb") as f:  # Download prefixes from Dropbox
+        metadata, res = dbx.files_download(path="/prefixes.json")
+        f.write(res.content)
+
     with open(PATH.joinpath("prefixes.json"), 'r') as f:
         prefixes = json.load(f)
 
@@ -49,9 +59,16 @@ async def on_guild_join(guild: discord.Guild):
     with open(PATH.joinpath("prefixes.json"), 'w') as f:
         json.dump(prefixes, f, indent=4)
 
+    with open(PATH.joinpath("prefixes.json"), "rb") as f:  # Upload prefixes to Dropbox
+        dbx.files_upload(f.read(), "/prefixes.json", mode=WriteMode('overwrite'))
+
 
 @bot.event
 async def on_guild_remove(guild: discord.Guild):
+    with open(PATH.joinpath("prefixes.json"), "wb") as f:  # Download prefixes from Dropbox
+        metadata, res = dbx.files_download(path="/prefixes.json")
+        f.write(res.content)
+
     with open(PATH.joinpath("prefixes.json"), 'r') as f:
         prefixes = json.load(f)
 
@@ -60,11 +77,17 @@ async def on_guild_remove(guild: discord.Guild):
     with open(PATH.joinpath("prefixes.json"), 'w') as f:
         json.dump(prefixes, f, indent=4)
 
+    with open(PATH.joinpath("prefixes.json"), "rb") as f:  # Upload prefixes to Dropbox
+        dbx.files_upload(f.read(), "/prefixes.json", mode=WriteMode('overwrite'))
+
 
 @bot.event
 async def on_ready():
     global guild_found
     guild_found = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
+    with open(PATH.joinpath("prefixes.json"), "wb") as f:  # Download prefixes from Dropbox
+        metadata, res = dbx.files_download(path="/prefixes.json")
+        f.write(res.content)
     print(
         f'{bot.user} is connected to the following guild:\n'
         f'{guild_found.name}(id: {guild_found.id})'
