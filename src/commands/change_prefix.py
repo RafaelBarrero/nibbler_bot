d9b7baff957820f4e04c02fec3df42c9d740ea78
RafaelBarrero <ramag874@gmail.com>
2021-03-06 15:31:53 +0100

    change_prefix.py: Changes to download and upload from Dropbox

--- src/commands/change_prefix.py
+++ src/commands/change_prefix.py
@@ -3,6 +3,9 @@ import json
 import pathlib
 from discord.ext import commands
 from discord.ext.commands import Context, has_permissions, MissingPermissions
+from dropbox.files import WriteMode
+
+from src.main import dbx
 
 
 class ChangePrefix(commands.Cog):
@@ -14,6 +17,10 @@ class ChangePrefix(commands.Cog):
     @commands.command(name='prefijo', help='Cambia el prefijo')
     @has_permissions(administrator=True)
     async def prefix(self, ctx: Context, new_prefix: str):
+        with open(f'{self.PATH}/prefixes.json', "wb") as f:  # Download prefixes from Dropbox
+            metadata, res = dbx.files_download(path="/prefixes.json")
+            f.write(res.content)
+
         with open(f'{self.PATH}/prefixes.json', 'r') as f:
             prefixes = json.load(f)
 
@@ -22,11 +29,13 @@ class ChangePrefix(commands.Cog):
         with open(f'{self.PATH}/prefixes.json', 'w') as f:
             json.dump(prefixes, f, indent=4)
 
-        await self.bot.user.edit(display_name="asds")
+        with open(f'{self.PATH}/prefixes.json', "rb") as f:  # Upload prefixes to Dropbox
+            dbx.files_upload(f.read(), "/prefixes.json", mode=WriteMode('overwrite'))
+
         await ctx.send(f"Listo. Prefijo cambiado a \"{new_prefix}\" :D")
 
     @prefix.error
-    async def kick_error(self, ctx: Context, error):
+    async def prefix_error(self, ctx: Context, error):
         if isinstance(error, MissingPermissions):
             mention = ctx.message.author.mention
             await ctx.send(f"Lo siento {mention}, no tienes permisos para eso :(")
