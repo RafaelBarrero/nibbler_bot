3e3691d0c006a429bbdc06030694a6c7e3cd567a
RafaelBarrero <ramag874@gmail.com>
2022-03-08 23:27:22 +0100

    feat(gitano_100.py): add gitano_100.py to play gitano 100% audios

---
+++ src/commands/gitano_100.py
@@ -0,0 +1,34 @@
+import discord
+import pathlib
+from discord.ext import commands
+
+from src.commands.play_sounds import PlaySound
+
+
+class Gitano100(commands.Cog):
+    file_path = pathlib.Path(__file__)
+    sound_path = file_path.parent.parent.joinpath("canciones")
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.play = PlaySound(self.bot)
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author == self.bot.user:
+            return
+
+        if message.author.voice:
+            ctx = await self.bot.get_context(message)
+            file = ""
+            if any(word in message.content.lower() for word in ['tienen miedo']):
+                file = self.sound_path.joinpath("gitano/miedo.mp3")
+            elif any(word in message.content.lower() for word in ['quieres 50 euro', 'quiere cincuenta euro',
+                                                                  'quiere 50 euro', 'quiereh cincuentah euroh',
+                                                                  'quiereh 50 euro', '50 euroh']):
+                file = self.sound_path.joinpath("gitano/euro.mp3")
+            await self.play.play_sound(ctx, file)
+
+
+def setup(bot):
+    bot.add_cog(Gitano100(bot))
