c42afa56fbc86f5e3edefdeeb065904108e20021
RafaelBarrero <ramag874@gmail.com>
2022-03-15 21:36:31 +0100

    fix(gitano_100.py): fix bot entering voice chat when writing on discord server

--- src/commands/gitano_100.py
+++ src/commands/gitano_100.py
@@ -18,16 +18,16 @@ class Gitano100(commands.Cog):
         if message.author == self.bot.user:
             return
 
-        if message.author.voice:
-            ctx = await self.bot.get_context(message)
-            file = ""
-            if any(word in message.content.lower() for word in ['tienen miedo']):
-                file = self.sound_path.joinpath("gitano/miedo.mp3")
-            elif any(word in message.content.lower() for word in ['quieres 50 euro', 'quiere cincuenta euro',
-                                                                  'quiere 50 euro', 'quiereh cincuentah euroh',
-                                                                  'quiereh 50 euro', '50 euroh']):
-                file = self.sound_path.joinpath("gitano/euro.mp3")
-            await self.play.play_sound(ctx, file)
+        ctx = await self.bot.get_context(message)
+        if any(word in message.content.lower() for word in ['tienen miedo']):
+            file = self.sound_path.joinpath("gitano/miedo.mp3")
+        elif any(word in message.content.lower() for word in ['quieres 50 euro', 'quiere cincuenta euro',
+                                                              'quiere 50 euro', 'quiereh cincuentah euroh',
+                                                              'quiereh 50 euro', '50 euroh']):
+            file = self.sound_path.joinpath("gitano/euro.mp3")
+        else:
+            return
+        await self.play.play_sound(ctx, file)
 
 
 def setup(bot):
