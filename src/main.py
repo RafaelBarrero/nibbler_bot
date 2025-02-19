cb0cbd4625d2834cb7f7b0bead3f1fb76fcd943e
RafaelBarrero <ramag874@gmail.com>
2025-02-19 21:09:35 +0100

    feat(main.py): get GUILD from environment variable

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 DROPBOX_TOKEN = os.getenv('DROPBOX_TOKEN')
-GUILD = "SALAS BORRACHAS"
+GUILD = os.getenv('GUILD')
 PATH = pathlib.Path(__file__).parent.parent
 intents = discord.Intents.all()
 guild_found: discord.Guild = None
